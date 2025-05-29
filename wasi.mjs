f4afcf5ea9cac279064160e0d52b14b4f04256c8
Vasilii Rogin <vasya@rogin.ru>
2025-05-29 16:38:07 +0300

    Add example runner using WASI

---
+++ wasi.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import { readFileSync } from "fs";
+import { WASI } from "wasi";
+import path from "path";
+
+process.removeAllListeners("warning").on("warning", (err) => {
+  if (
+    err.name !== "ExperimentalWarning" &&
+    !err.message.includes("experimental")
+  ) {
+    console.warn(err);
+  }
+});
+
+const wasi = new WASI({
+  version: "preview1",
+  args: process.argv.slice(1),
+  env: process.env,
+  preopens: {
+    "/": "/",
+  },
+});
+
+(async () => {
+  const wasm = await WebAssembly.compile(
+    readFileSync(path.join(import.meta.dirname, "sslc.wasm"))
+  );
+  const instance = await WebAssembly.instantiate(wasm, wasi.getImportObject());
+
+  wasi.start(instance);
+})();
